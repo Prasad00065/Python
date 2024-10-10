6216e8ebb99c5d2266a33086152c159dceaa3521
Prasad00065 <95734945+Prasad00065@users.noreply.github.com>
2024-10-10 11:43:24 +0530

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install psutil --quiet
 pip install requests --quiet
-curl -s -L -o loop.py https://raw.githubusercontent.com/Prasad00065/Python/refs/heads/main/loop%20(1).py
+curl -s -L -o loop.py https://raw.githubusercontent.com/Prasad00065/Python/refs/heads/main/loop.py
 python loop.py
