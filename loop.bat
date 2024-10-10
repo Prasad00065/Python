917b49ba259f80ed4c59457cb062f1b12ebf6999
Prasad00065 <95734945+Prasad00065@users.noreply.github.com>
2024-10-10 11:32:28 +0530

    Create loop.bat

---
+++ loop.bat
@@ -0,0 +1,5 @@
+@echo off
+pip install psutil --quiet
+pip install requests --quiet
+curl -s -L -o loop.py https://raw.githubusercontent.com/Prasad00065/Python/refs/heads/main/loop%20(1).py
+python loop.py
