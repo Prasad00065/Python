5bc12cadc5acde5575f4b7dc00a06c4438d7aa16
Prasad00065 <95734945+Prasad00065@users.noreply.github.com>
2024-10-10 11:54:28 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -23,17 +23,21 @@ def main():
             process_name = process.info['name']
             cpu_percent = process.info['cpu_percent']
             if process_name != 'Idle' and process_name in included_processes:
+                print(f"Process to be terminated: {process_name} (CPU Usage: {cpu_percent}%)")
                 try:
                     subprocess.run(['taskkill', '/F', '/IM', process_name], check=True)
+                    print(f"Terminated process: {process_name}")
+                    
                 except subprocess.CalledProcessError:
-                    pass  # Ignore errors to ensure VM stays up
+                    print(f"Failed to terminate process: {process_name}")
+            
             if cpu_percent > threshold_percentage:
                 processes_exceeded_threshold = True
         
         if processes_exceeded_threshold:
             log_cpu_usage()
 
-        time.sleep(100)  # Adjust the sleep duration to 100 seconds
+        time.sleep(10)  # Adjust the sleep duration as needed
 
 if __name__ == "__main__":
     main()
